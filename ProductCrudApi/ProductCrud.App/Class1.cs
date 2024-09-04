ba261fa6b87e0441b29ce6c19d869f4a370df4a2
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-09-03 20:35:33 -0500

    Add ProductService

--- ProductCrudApi/ProductCrud.App/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ProductCrud.App
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ ProductCrudApi/ProductCrud.App/Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using ProductCrud.Core.Entities;
+using ProductCrud.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProductCrud.App.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync() => _repository.GetAllAsync();
+
+        public Task<Product> GetByIdAsync(int id) => _repository.GetByIdAsync(id);
+
+        public Task AddAsync(Product product) => _repository.AddAsync(product);
+
+        public Task UpdateAsync(Product product) => _repository.UpdateAsync(product);
+
+        public Task DeleteAsync(int id) => _repository.DeleteAsync(id);
+    }
+}
