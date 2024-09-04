1a41ad713507564c74f59f2d1e4588b719ff5fce
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-09-03 20:47:44 -0500

    Configure services in program

--- ProductCrudApi/ProductCrud.Api/Program.cs
+++ ProductCrudApi/ProductCrud.Api/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ProductCrud.App.Services;
+using ProductCrud.Core.Interfaces;
+using ProductCrud.Infra.Data;
+using ProductCrud.Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configure SQL Server
+IServiceCollection serviceCollection = builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services dependency inyection
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ProductService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
